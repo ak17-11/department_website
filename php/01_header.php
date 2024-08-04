06ec2560040fd9a91a5dfece2a80fe22ed30e467
ak17-11 <65407040+ak17-11@users.noreply.github.com>
2024-08-04 15:50:48 +0530

    Create 01_header.php

---
+++ php/01_header.php
@@ -0,0 +1,164 @@
+<header>
+      <div class="container-fluid m-0 p-0">
+        <!-- Logo Navbar -->
+        <nav class="navbar navbar-white bg-white">
+          <div class="container-fluid d-flex flex-column align-items-center flex-md-row justify-content-md-between px-4">
+            <img
+              src="images/dcsa.jpg"
+              class="rounded float-start"
+              height="80"
+              alt="Department Logo"
+            />
+            <div class="m-3 mb-1 text-shadow d-flex flex-column align-items-sm-center align-items-md-start">
+              <h3 id="dept-name">
+                UGC-Centre of Excellence in Applications of Nanomaterials,
+                Nanoparticles & Nanocomposites
+                <!-- SC/ST CELL -->
+              </h3>
+              <h6 id="uni-name" class="">PANJAB UNIVERSITY</h6>
+            </div>
+            <img
+              src="images/pu.jpeg"
+              class="rounded float-end d-none d-lg-inline"
+              height="80"
+              alt="PU Logo"
+            />
+          </div>
+        </nav>
+      </div>
+    </header>
+
+    <nav class="navbar navbar-dark navbar-expand-md sticky-top base-color p-0 shadow">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="navbar-nav justify-content-evenly w-100">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle text-white text-shadow"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                data-bs-auto-close="outside"
+              >
+                <i class="fas fa-lightbulb pe-1"></i>About
+              </a>
+              <ul class="dropdown-menu text-black">
+                <li><a class="  " href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle text-white text-shadow"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                data-bs-auto-close="outside"
+              >
+                <i class="fas fa-book pe-1"></i>Academics
+              </a>
+              <ul class="dropdown-menu text-black">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle text-white text-shadow"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                data-bs-auto-close="outside"
+              >
+                <i class="fas fa-magnifying-glass pe-1"></i>Research
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle text-white text-shadow"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                data-bs-auto-close="outside"
+              >
+                <i class="fas fa-calendar pe-1"></i>Activity
+              </a>
+              <ul class="dropdown-menu text-black">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle text-white text-shadow"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                data-bs-auto-close="outside"
+              >
+                <i class="fas fa-briefcase pe-1"></i>Career
+              </a>
+              <ul class="dropdown-menu text-black">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle text-white text-shadow"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                data-bs-auto-close="outside"
+                aria-expanded="false"
+              >
+                <i class="fas fa-list pe-1"></i>Notices
+              </a>
+              <ul class="dropdown-menu text-black">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
